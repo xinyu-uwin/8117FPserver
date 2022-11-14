816fc440eb006be409009fc35bb96bb152a5e006
venkata-naveen-varma-v <114001073+venkata-naveen-varma-v@users.noreply.github.com>
2022-11-14 11:22:05 -0500

    FP-62

--- iot_devices/device_light_bulb/light.py
+++ iot_devices/device_light_bulb/light.py
@@ -6,12 +6,18 @@ def customCallback(client,userdata,message):
 	data = json.loads(message.payload)
 	# print("Data received from Server: ", data)
 	time = datetime.datetime.now()
+
+	username = data["username"]
+	room_name = data["room_name"]
+	print(">>> In room: " + room_name + " of user: " + username, end="\n")
+	
 	print(time, end=" ")
 	light_on_percent = data["light-on"]
 	if light_on_percent == 0:
 		print("--- LIGHT TURNED OFF")
 	else:
 		print("--- LIGHT TURNED ON Percentage = ", light_on_percent)
+	print("\n")
 
 myMQTTClient = AWSIoTMQTTClient("light_bulb")
 myMQTTClient.configureEndpoint("a32yk77mbrevmu-ats.iot.us-east-2.amazonaws.com", 8883)
