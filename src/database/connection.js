4b4abac1a93f89c76100abb9535b4955eca4c2df
venkata-naveen-varma-v <114001073+venkata-naveen-varma-v@users.noreply.github.com>
2022-11-17 19:10:59 -0500

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,11 @@
+/**
+ * import postgresql library
+ */
 const {Client} = require('pg')
 
-// For localhost DB
-// const client_dev = new Client({
-//     host            : process.env.DB_HOST,
-//     user            : process.env.DB_USER,
-//     port            : process.env.DB_PORT,
-//     password        : process.env.DB_PASS,
-//     database        : process.env.DB_NAME
-// })
-
-// For heroku DB
+/**
+ * Connect to Heroku database
+ */
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
